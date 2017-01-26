cd9f5d2075c24a800683f5a1e8c24179ac51ce58
greensounds <jesus.diaz@cide.edu>
2017-01-26 14:56:37 -0600

    Agregar relojes

--- js/clocks.js
+++ js/clocks.js
@@ -3,20 +3,20 @@ window.onload=function(){
     var clockA = document.getElementById("countdownPageA")
     var targetDateA = new Date(2013, 11, 10); // Dec 10, 2013;
     setInterval(function(){
-    	clockA.innerHTML = countdown(targetDateA).toString();
-	}, 1000);
+        clockA.innerHTML = countdown(targetDateA).toString();
+    }, 1000);
     //Reloj para Ley General en materia de desapariciÃƒÆ’Ã‚Â³n forzada
-	var clockB = document.getElementById("countdownPageB")
+    var clockB = document.getElementById("countdownPageB")
     var targetDateB = new Date(2016, 00, 06); // Enero 06, 2016;
     setInterval(function(){
-   		clockB.innerHTML = countdown(targetDateB).toString();
-	}, 1000);
-    //Reloj para Integrante de la Junta del INEGI
-	var clockC = document.getElementById("countdownPageC")
-    var targetDateC = new Date(2016, 11, 31); // Dic 31 2016;
+        clockB.innerHTML = countdown(targetDateB).toString();
+    }, 1000);
+    //Reloj para Ley de Unidad de Medida
+    var clockC = document.getElementById("countdownPageC")
+    var targetDateC = new Date(2016, 04, 26); // May 26, 2016;
     setInterval(function(){
-    	clockC.innerHTML = countdown(targetDateC).toString();
-	}, 1000);
+        clockC.innerHTML = countdown(targetDateC).toString();
+    }, 1000);
     //Reloj para Ley Federal de Competencia econÃ³mica
     var clockD = document.getElementById("countdownPageD")
     var targetDate = new Date(2015, 06, 06); // 31 de enero de 2015;
@@ -24,83 +24,83 @@ window.onload=function(){
         clockD.innerHTML = countdown(targetDate).toString();
     }, 1000);
     //Reloj para Ley de Registro PÃƒÆ’Ã‚Âºblicos Inmobiliarios
-	var clockF = document.getElementById("countdownPageF")
+    var clockF = document.getElementById("countdownPageF")
     var targetDateF = new Date(2014, 05, 27); // Jun 27, 2014;
     setInterval(function(){
-    	clockF.innerHTML = countdown(targetDateF).toString();
-	}, 1000);
+        clockF.innerHTML = countdown(targetDateF).toString();
+    }, 1000);
     //Reloj para Registro Nacional de PoblaciÃƒÆ’Ã‚Â³n:
-	var clockH = document.getElementById("countdownPageH")
+    var clockH = document.getElementById("countdownPageH")
     var targetDateH = new Date(2014, 11, 17); // Dec 17, 2014;
     setInterval(function(){
-    	clockH.innerHTML = countdown(targetDateH).toString();
-	}, 1000);
+        clockH.innerHTML = countdown(targetDateH).toString();
+    }, 1000);
     //Reloj para Bases para la coordinaciÃƒÆ’Ã‚Â³n entre los poderes federales y los poderes locales de la Ciudad de MÃƒÆ’Ã‚Â©xico 
-	var clockI = document.getElementById("countdownPageI")
+    var clockI = document.getElementById("countdownPageI")
     var targetDateI = new Date(2017, 00, 31); // 31 enero 2017;
     setInterval(function(){
-    	clockI.innerHTML = countdown(targetDateI).toString();
-	}, 1000);
+        clockI.innerHTML = countdown(targetDateI).toString();
+    }, 1000);
     //Reloj para Propaganda gubernamental
-	var clockJ = document.getElementById("countdownPageJ")
+    var clockJ = document.getElementById("countdownPageJ")
     var targetDateJ = new Date(2007, 11, 14); // Dec 14, 2007;
     setInterval(function(){
-    	clockJ.innerHTML = countdown(targetDateJ).toString();
-	}, 1000);
+        clockJ.innerHTML = countdown(targetDateJ).toString();
+    }, 1000);
     //Reloj para: Ley sobre suspensiÃƒÆ’Ã‚Â³n de derechos y garantÃƒÆ’Ã‚Â­as:
-	var clockK = document.getElementById("countdownPageK")
+    var clockK = document.getElementById("countdownPageK")
     var targetDateK = new Date(2012, 05, 11); // Jun 11, 2012;
     setInterval(function(){
-    	clockK.innerHTML = countdown(targetDateK).toString();
-	}, 1000);
+        clockK.innerHTML = countdown(targetDateK).toString();
+    }, 1000);
     //Reloj para Magistrado del TEPJF
-	var clockL = document.getElementById("countdownPageL")
+    var clockL = document.getElementById("countdownPageL")
     var targetDateL = new Date(2015, 03, 20); // Apr 20, 2015;
     setInterval(function(){
-    	clockL.innerHTML = countdown(targetDateL).toString();
-	}, 1000);
+        clockL.innerHTML = countdown(targetDateL).toString();
+    }, 1000);
     //Reloj para Ley del artÃƒÆ’Ã‚Â­culo 33, extranjeros:
-	var clockM = document.getElementById("countdownPageM")
+    var clockM = document.getElementById("countdownPageM")
     var targetDateM = new Date(2012, 05, 11); // Jun 11, 2012;
     setInterval(function(){
-    	clockM.innerHTML = countdown(targetDateM).toString();
-	}, 1000);
+        clockM.innerHTML = countdown(targetDateM).toString();
+    }, 1000);
     //Reloj para Ley General de Aguas
-	var clockO = document.getElementById("countdownPageO")
+    var clockO = document.getElementById("countdownPageO")
     var targetDateO = new Date(2013, 01, 03); // Feb 03, 2013;
     setInterval(function(){
-    	clockO.innerHTML = countdown(targetDateO).toString();
-	}, 1000);
+        clockO.innerHTML = countdown(targetDateO).toString();
+    }, 1000);
     //Reloj para Ley del Coneval
-	var clockP = document.getElementById("countdownPageP")
+    var clockP = document.getElementById("countdownPageP")
     var targetDateP = new Date(2014, 05, 11); // Jun 11, 2014;
     setInterval(function(){
-    	clockP.innerHTML = countdown(targetDateP).toString();
-	}, 1000);
+        clockP.innerHTML = countdown(targetDateP).toString();
+    }, 1000);
     //Reloj para Consejo General del Coneval
-	var clockQ = document.getElementById("countdownPageQ")
+    var clockQ = document.getElementById("countdownPageQ")
     var targetDateQ = new Date(2014, 03, 12); // Apr 12, 2014;
     setInterval(function(){
-    	clockQ.innerHTML = countdown(targetDateQ).toString();
-	}, 1000);
+        clockQ.innerHTML = countdown(targetDateQ).toString();
+    }, 1000);
     //Reloj para Ley General del Sistema Nacional de Archivos:
-	var clockR = document.getElementById("countdownPageR")
+    var clockR = document.getElementById("countdownPageR")
     var targetDateR = new Date(2015, 01, 07); // Feb 07, 2015;
     setInterval(function(){
-    	clockR.innerHTML = countdown(targetDateR).toString();
-	}, 1000);
+        clockR.innerHTML = countdown(targetDateR).toString();
+    }, 1000);
     //Reloj para Ley Federal de ProtecciÃƒÆ’Ã‚Â³n de Datos Personales en PosesiÃƒÆ’Ã‚Â³n de Particulares:
-	var clockS = document.getElementById("countdownPageS")
+    var clockS = document.getElementById("countdownPageS")
     var targetDateS = new Date(2015, 01, 07); // Feb 07, 2015;
     setInterval(function(){
-    	clockS.innerHTML = countdown(targetDateS).toString();
-	}, 1000);
+        clockS.innerHTML = countdown(targetDateS).toString();
+    }, 1000);
     //Reloj para Ley de coordinaciÃƒÆ’Ã‚Â³n administrativa en materia
-	var clockT = document.getElementById("countdownPageT")
+    var clockT = document.getElementById("countdownPageT")
     var targetDateT = new Date(2017, 00, 31); // Enero 31, 2017;
     setInterval(function(){
-    	clockT.innerHTML = countdown(targetDateT).toString();
-	}, 1000);
+        clockT.innerHTML = countdown(targetDateT).toString();
+    }, 1000);
 
 //reloj para "Ley Nacional en Materia de Ejecuciones"
     var clockU = document.getElementById("countdownPageU")
@@ -193,19 +193,5 @@ var clockV = document.getElementById("countdownPageV")
     var targetDateA9 = new Date(2013, 11, 10); // Dec 10 2013
     setInterval(function(){
         clockA9.innerHTML = countdown(targetDateA9).toString();
-    }, 1000);
-
-    //Reloj para Integrante de la Junta del Banco de México
-    var clockA10 = document.getElementById("countdownPageA10")
-    var targetDateA10 = new Date(2016, 11, 31); // Dic 31 2016;
-    setInterval(function(){
-        clockC.innerHTML = countdown(targetDateA10).toString();
-    }, 1000);}
-
-    //Reloj para Integrante de la Junta del Banco de México
-    var clockA11 = document.getElementById("countdownPageA11")
-    var targetDateA11 = new Date(2016, 11, 31); // Dic 31 2016;
-    setInterval(function(){
-        clockC.innerHTML = countdown(targetDateA11).toString();
     }, 1000);
 }
