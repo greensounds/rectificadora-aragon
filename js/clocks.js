1269ac6967698db256a6182af6e5d77caf55f066
greensounds <jesus.diaz@cide.edu>
2017-02-28 10:11:31 -0600

    Actualizar

--- js/clocks.js
+++ js/clocks.js
@@ -57,7 +57,7 @@ window.onload=function(){
     }, 1000);
     //Reloj para Magistrado del Justicia Laboral
     var clockL = document.getElementById("countdownPageL")
-    var targetDateL = new Date(2018, 01, 25); // Febrero 25, 2018;
+    var targetDateL = new Date(2017, 01, 25); // Febrero 25, 2018;
     setInterval(function(){
         clockL.innerHTML = countdown(targetDateL).toString();
     }, 1000);
